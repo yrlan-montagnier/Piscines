9495fbaf89f40c6e85c106854f68a6f4f288fc53
Yrlan <71253160+yrlan-montagnier@users.noreply.github.com>
2021-11-29 11:34:38 +0100

    Update

--- multiplication-table.php
+++ multiplication-table.php
@@ -2,14 +2,10 @@
 
 $num = 10;
 
-for($i = 1; $i <= 10; $i++)
-{	
-	for($j =1; $j <= $num; $j++)
-	{
+for($i = 1; $i <= 10; $i++) {	
+	for($j =1; $j <= $num; $j++) {
 		$multiplication_table = ($i * $j);
 		echo "$j  x $i =  $multiplication_table";
 	}
-	
 	echo "<tr/>";
 }
-?>
