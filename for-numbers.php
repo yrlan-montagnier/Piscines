7cdd6fac44b839e0bc7cc45fc074ff863ce4488c
Yrlan <71253160+yrlan-montagnier@users.noreply.github.com>
2021-11-29 15:44:26 +0100

    Update

--- for-numbers.php
+++ for-numbers.php
@@ -1,12 +1,12 @@
 <?php  
 $num = 2;
+
 while ($num <= 100 ) {  
     $div_count=0;  
+
     for ($i=1; $i<=$num; $i++) { if (($num%$i)==0) { $div_count++; } } 
     if ($div_count<3 && $num !=97) { echo "$num , "; $div_count++; }
-    else if ($div_count<3 && $num = 97) {
-        echo $num;
-        $div_count++;
-    }
+    if ($div_count<3 && $num = 97) { echo $num; $div_count++; }
+
     $num=$num+1;  
 }
