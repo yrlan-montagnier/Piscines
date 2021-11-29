25f512085c89bfe9035d84b9847839d9530465fb
Yrlan <71253160+yrlan-montagnier@users.noreply.github.com>
2021-11-29 17:12:41 +0100

    Update

---
+++ what-month-is-it.php
@@ -0,0 +1,42 @@
+<?php
+$month = date('M');
+
+switch ($month) {
+    case 1:
+        echo 'We are in the month of January';
+        break;
+    case 2:
+        echo 'We are in the month of February';
+        break;
+    case 3:
+        echo 'We are in the month of March';
+        break;
+    case 4:
+        echo 'We are in the month of April';
+        break;
+    case 5:
+        echo 'We are in the month of May';
+        break;
+    case 6:
+        echo 'We are in the month of June';
+        break;
+    case 7:
+        echo 'We are in the month of July';
+        break;
+    case 8:
+        echo 'We are in the month of August';
+        break;
+    case 9:
+        echo 'We are in the month of September';
+        break;
+    case 10:
+        echo 'We are in the month of October';
+        break;
+    case 11:
+        echo 'We are in the month of November';
+        break;
+    case 12:
+        echo 'We are in the month of December';
+        break;
+}
+?>
