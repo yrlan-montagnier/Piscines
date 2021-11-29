18c766dd59e1185e68b3454900f751eb5f379aba
Yrlan <71253160+yrlan-montagnier@users.noreply.github.com>
2021-11-29 14:29:43 +0100

    Update

--- its-ricard-time.php
+++ its-ricard-time.php
@@ -6,10 +6,10 @@ do {
     echo("Come on ricard number $nbrRicard \n");
     $nbrRicard++;
 
-    if($nbrRicard == 3) {
+    if($nbrRicard == 4) {
         echo("I'll have to stop soon! \n");
     } 
-    if($nbrRicard == 5  ) {
+    if($nbrRicard == 6  ) {
         echo("I am no longer very fresh ... \n");
     }
 } while($nbrRicard <= 7);
