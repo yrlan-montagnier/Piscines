60a14ce2655a72b66e6a0dfb7ea091db78dd649a
Yrlan <71253160+yrlan-montagnier@users.noreply.github.com>
2021-11-29 09:56:16 +0100

    Update

---
+++ primitive-declarations.php
@@ -0,0 +1,9 @@
+<?php
+
+$firstStr = "Now I am assigned a string! Fabulous.";
+$firstEscapeStr = "Now I am assigned a string! Fabulous.";
+$firstBool = true;
+$firstInteger += 1;
+$firstFloat += 1.5;
+var_dump($firstNull);
+$unset_arr[3] = "def";
