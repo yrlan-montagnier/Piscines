39272d7f6662132857ec139ac7974798b3727f24
Yrlan <71253160+yrlan-montagnier@users.noreply.github.com>
2021-11-29 19:19:58 +0100

    Update

--- pangram.php
+++ pangram.php
@@ -1,10 +1,8 @@
 <?php
 
 function isPangram(string $str): bool {
-    $newStr = preg_replace('/\s+/', '', $str);
     $pattern = '/[A-Z]/i';
-
-    echo preg_match($pattern, $newStr);
-    return preg_match($pattern, $newStr);
+    return preg_match($pattern, $str);
 }
+
 isPangram('the quick brown fox jumps over the lazy dog');
