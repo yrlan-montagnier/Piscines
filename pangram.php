98b9dfc76ba96531bcfbdf8b48589c1dfbb5608c
Yrlan <71253160+yrlan-montagnier@users.noreply.github.com>
2021-11-30 11:15:12 +0100

    Update

--- pangram.php
+++ pangram.php
@@ -1,12 +1,22 @@
 <?php
 
 function isPangram(string $str): bool {
-    $pattern = '/[A-Z]/i';
+    $new_str = str_replace(' ', '', $str);
+    echo $new_str;
 
-    if (preg_match($pattern, $str))
+    $pattern = "/[A-Z]/i";
+    $match = preg_match_all($pattern, $new_str);
+
+    echo preg_match($pattern, $new_str);
+
+    if ($match) {
         return 1;
-    else 
+        echo "Pangram";
+    }
+    else {
         return 0;
+        echo "Pas un pangram";
+    }
 }
 
 isPangram('the quick brown fox jumps over the lazy dog');
---
+++ test.php
@@ -0,0 +1,24 @@
+<?php
+
+function isPangram($str)
+{
+    $alphabet = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
+    $isPangram = false;
+    $strArray = str_split($str);
+
+    foreach ($strArray as $char) {
+        if (ctype_alpha($char)) {
+            if (ctype_upper($char)) {
+                $char = strtolower($char);
+            }
+            $key = array_search($char, $alphabet);
+            if ($key !== false) {
+                unset($alphabet[$key]);
+            }
+        }
+    }
+    if (!$alphabet) {
+        $isPangram = true;
+    }
+    return $isPangram;
+}
