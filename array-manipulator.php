17c592e9d4495326c24251551fbb20444f2e6842
Yrlan <71253160+yrlan-montagnier@users.noreply.github.com>
2021-12-06 10:20:26 +0100

    Update

--- array-manipulator.php
+++ array-manipulator.php
@@ -3,4 +3,12 @@
 function reverse(array $arr): array {
     return array_reverse($arr);
 }
-reverse(['One', 'Two', 'Three']);
+
+function push(array $arr, string $str): array {
+    $arr[] = $str;
+    return $arr;
+}
+
+function sum(array $arr): int {
+    return array_sum($arr);
+}
