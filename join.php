d4aa12d28a20849de07f04fd1c704219387612a5
Yrlan <71253160+yrlan-montagnier@users.noreply.github.com>
2021-12-07 15:10:49 +0100

    Update

--- join.php
+++ join.php
@@ -1,12 +1,13 @@
 <?php
+
 function joinWords(array $arr, string $separator = ' '): string {
     $str = "";       
 
-    foreach( $arr as $key => $value ){
+    foreach( $arr as $value ){
         $str = $value.$separator;
         echo $str;
-        return $str;
     }
+    return $str;
 }
 
 joinWords(['La', 'fonction', 'join'], ' ');
